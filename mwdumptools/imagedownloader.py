2b355927be20bb19e85558e4652566fd22dbfbfd
Benjamin Bach <benjaoming@gmail.com>
2015-01-20 22:05:15 -0800

    new executable and make downloader work with list of URL candidates

--- mwdumptools/imagedownloader.py
+++ mwdumptools/imagedownloader.py
@@ -5,7 +5,10 @@ python-mwdump-tools - imagedownloader
 =====================================
 
 Naively downloads images by parsing a Mediawiki dump and reading all the
-File:XXX articles.
+File:XXX articles. Reads from STDIN.
+
+Example:
+  imagedownloader --output=images/ --scale < tests/data/ngwiki-20130702-pages-articles-multistream.xml
 
 Uses concurrency to download and scale images.
 
@@ -15,19 +18,20 @@ all images.
 
 
 Usage:
-  imagedownloader [--dlurl=URL]
+  imagedownloader [--dlurls=URL]...
                   [--output=PATH]
                   [--scale]
                   [--ext=EXT]...
                   [--resume=N]
                   [--namespaces=NS]...
+                  [--treads=N]
   imagedownloader (-h | --help)
   imagedownloader --version
 
 Options:
   -h --help          Show this screen.
   --version          Show version.
-  --dlurl=URL        The root URL from where to find images. Python string
+  --dlurls=URL       The root URL from where to find images. Python string
                      formatting is required! Example:
                      "http://upload.wikimedia.org/wikipedia/commons/{h1:s}/{h2:s}/{fname:s}"
   --savepath=PATH    Where to save images
@@ -37,7 +41,12 @@ Options:
                      [default: jpg, jpeg, png, gif, bmp, tiff]
   --resume=N         Resume from line no (if the script has been interrupted)
   --namespaces=NS    The mediawiki dump namespace to read from
-                     [default: 6]
+                     [default: 0]
+  --threads=N        Number of concurrent threads that should download and scale
+                     images (most likely you should set this to the number of
+                     CPU cores but depending on how slow your internet connection
+                     is, you can set it higher).
+                     [default: 8]
 """
 import concurrent.futures
 from hashlib import md5
@@ -61,8 +70,10 @@ from . import streamparser
 ################################################################
 # Default values, set using kwargs in ImageDownloader.__init__ #
 ################################################################
-DEFAULT_DOWNLOAD_PATH = "http://upload.wikimedia.org/wikipedia/commons/{h1:s}/{h2:s}/{fname:s}"
+DEFAULT_DOWNLOAD_PATHS = ["http://upload.wikimedia.org/wikipedia/commons/{h1:s}/{h2:s}/{fname:s}"]
 GET_EXTENSIONS = [".jpg", "jpeg", "png", "gif", "bmp"]
+
+DEFAULT_NAMESPACES = ["0"]
 
 DEFAULT_THUMBNAIL_PATH = "http://upload.wikimedia.org/wikipedia/commons/{h1:s}/{h2:s}/{fname:s}"
 
@@ -77,24 +88,23 @@ MAX_IMAGE_SIZE = (1024, 1024)
 # can increase the number.
 # The higher your bandwidth, the less threads you want, because they
 # finish faster!!
-MAX_THREADS = 8
+DEFAULT_MAX_THREADS = 8
 
-# Output
+# Output SQL for mediawiki images insert to STDOUT
 OUTPUT_SQL = True
 
-SQL_UPDATE = (
-    "UPDATE image "
-    "SET img_width='{width:d}', img_height='{height:d}', img_size='{filesize}' "
-    "WHERE img_name='{name:s}'"
+SQL_VALUES = (
+    "('{name:s}', '{width:d}', '{height:d}', '{filesize}'),\n"
 )
 
 DOWNLOAD_TIMEOUT = 10  # seconds
 
 DOWNLOAD_RETRIES = 2
 
+SKIP_EXISTING = True
+
+
 # Retrieve a single page and report the url and contents
-
-
 def load_url(url, local_path, timeout):
     for __ in range(DOWNLOAD_RETRIES):
         try:
@@ -108,10 +118,10 @@ def load_url(url, local_path, timeout):
             return local_path
         except urllib.error.URLError:
             continue  # DNS error
-        except urllib.error.HTTPError:
-            return None  # Probably 404, so do not retry
         except socket.gaierror:
             continue  # Network error
+        except urllib.error.HTTPError:
+            raise
 
 
 # Scale image bytes and save to local_path
@@ -150,36 +160,48 @@ def job(fn):
 # raises a BaseException subclass, the behavior is undefined.
 class ImagePoolWorker:
 
-    def __init__(self, processes, dlurl, output_dir,
+    def __init__(self, processes, dlurls, output_dir,
                  max_image_size, timeout, output_stream):
         self.processes = processes
         self.executor = concurrent.futures.ProcessPoolExecutor(
             max_workers=processes)
         self.jobs_running = 0
-        self.dlurl = dlurl
+        self.dlurls = dlurls
         self.output_dir = output_dir
         self.max_image_size = max_image_size
         self.timeout = timeout
         self.max_processes = processes
         self.output_lock = threading.Lock()
         self.output_stream = output_stream
-
+    
+    def get_images(self, urls, fname, local_path, timeout, callback, error_callback):
+        """Try a series of URLs"""
+        if SKIP_EXISTING and os.path.exists(local_path):
+            settings.logger.debug("{:s} exists, skipping".format(local_path))
+            self.image_resized(fname, local_path)
+            return
+        else:
+            self.get_image(urls, 0, fname, local_path, timeout, callback, error_callback)
+    
     @job
-    def get_image(self, url, fname, local_path, timeout, callback, error_callback):
-        settings.logger.debug("Downloading {:s}".format(url))
+    def get_image(self, urls, index, fname, local_path, timeout, callback, error_callback):
+        settings.logger.debug("Downloading {:s}".format(urls[index]))
         try:
-            future = self.executor.submit(load_url, url, local_path, timeout)
+            future = self.executor.submit(load_url, urls[index], local_path, timeout)
             future.add_done_callback(
-                lambda future: callback(fname, future.result(), url))
+                lambda future: callback(fname, local_path, future.result(), urls[index]))
         except Exception as exc:
-            error_callback(url, exc)
+            if index < len(urls):
+                self.get_image(urls, index + 1, fname, local_path, timeout, callback, error_callback)
+            else:
+                error_callback(fname, exc)
 
     @job
     def scale_image(self, fname, local_path, size, callback, error_callback):
         try:
             future = self.executor.submit(scale_image, fname, local_path, size)
             future.add_done_callback(
-                lambda future: callback(future, fname, local_path))
+                lambda future: callback(fname, local_path, future=future))
         except Exception as exc:
             error_callback(local_path, exc)
 
@@ -187,7 +209,7 @@ class ImagePoolWorker:
         time.sleep(timeout)
         self.executor.shutdown(wait=True)
 
-    def image_downloaded(self, fname, local_path, url):
+    def image_downloaded(self, fname, local_path, results, url):
         """Callback from WorkerThread"""
         self.scale_image(
             fname,
@@ -197,19 +219,23 @@ class ImagePoolWorker:
             self.image_resize_error
         )
 
-    def image_download_error(self, url, exception):
+    def image_download_error(self, fname, exception):
         """Callback from WorkerThread"""
-        settings.logger.error("Could not download: {0:s}".format(url))
+        settings.logger.error("Could not download: {0:s}".format(fname))
 
-    def image_resized(self, future, fname, local_path):
-        if future.exception() is not None:
-            settings.logger.error("got exception: %s" % future.exception())
-            return
-        size = future.result()
+    def image_resized(self, fname, local_path, future=None):
+        if future:
+            exc = future.exception()
+            if exc is not None:
+                settings.logger.error("Could not resize {:s}: {}".format(fname, exc))
+                return
+            size = future.result()
+        else:
+            size = os.path.getsize(local_path)
         if not OUTPUT_SQL:
             return
         self.output_lock.acquire()
-        self.output_stream.write(SQL_UPDATE.format(
+        self.output_stream.write(SQL_VALUES.format(
             width=size[0],
             height=size[1],
             filesize=os.stat(local_path).st_size,
@@ -232,21 +258,23 @@ class ImagePoolWorker:
 
 class ImageDownloader(streamparser.XmlStreamParser, ImagePoolWorker):
 
-    def __init__(self, in_file=None, output=OUTPUT_ROOT, namespaces=["6"],
-                 dlurl=DEFAULT_DOWNLOAD_PATH, max_image_size=MAX_IMAGE_SIZE,
-                 processes=MAX_THREADS, timeout=DOWNLOAD_TIMEOUT, **kwargs):
-
+    def __init__(self, dlurls=DEFAULT_DOWNLOAD_PATHS, in_file=None,
+                 output=OUTPUT_ROOT, namespaces=DEFAULT_NAMESPACES,
+                 max_image_size=MAX_IMAGE_SIZE, threads=DEFAULT_MAX_THREADS,
+                 timeout=DOWNLOAD_TIMEOUT, **kwargs):
+        
         self.namespaces = namespaces
-
+        self.output_stream = sys.stdout
         streamparser.XmlStreamParser.__init__(self, in_file=in_file,
                                               out_file=sys.stdout, **kwargs)
-        ImagePoolWorker.__init__(self, processes, dlurl, output,
-                                 max_image_size, timeout, sys.stdout)
+        ImagePoolWorker.__init__(self, threads, dlurls, output,
+                                 max_image_size, timeout, output_stream=self.output_stream)
 
     def parse_site_info(self, lines):
         streamparser.XmlStreamParser.parse_site_info(self, lines)
 
     def handle_page(self, page):
+        raise NotImplementedError("This function should not use the title tag but rather scan the article text for any usage of [[File]]")
         if page.find("ns").text in self.namespaces:
             try:
                 title = page.find("title").text
@@ -256,10 +284,13 @@ class ImageDownloader(streamparser.XmlStreamParser, ImagePoolWorker):
             fname = title.replace("File:", "")
             h1, h2 = self.get_hash(fname)
             local_path = self.get_local_path(h1, h2, fname)
-            url = DEFAULT_DOWNLOAD_PATH.format(h1=h1, h2=h2, fname=fname)
-            settings.logger.debug("Trying to get: {}".format(url))
-            self.get_image(
-                url,
+            urls = list(map(
+                lambda s: s.format(h1=h1, h2=h2, fname=fname),
+                DEFAULT_DOWNLOAD_PATHS
+            ))
+            settings.logger.debug("Started download job for: " + fname)
+            self.get_images(
+                urls,
                 fname,
                 local_path,
                 self.timeout,
@@ -268,12 +299,14 @@ class ImageDownloader(streamparser.XmlStreamParser, ImagePoolWorker):
             )
 
     def execute(self):
+        if OUTPUT_SQL:
+            self.output_stream.write("INSERT INTO image(img_name, img_width, img_height, img_size) VALUES \n")
         streamparser.XmlStreamParser.execute(self)
         self.shutdown(self.timeout)
 
 
 if __name__ == "__main__":
-    arguments = docopt(__doc__, version='python-mw-tools ' + str(VERSION))
+    arguments = docopt(__doc__, version='python-mw-tools imagedownloader ' + str(VERSION))
     arguments = dict((k.replace("--", ""), v) for k, v in arguments.items())
     arguments = dict(
         filter(lambda kv: not kv[1] is None, [(k, v) for k, v in arguments.items()]))
